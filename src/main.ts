c99d45c8b40f2ec6482ee8c9b51575c877d6f385
nammob <hoangnam@mobcec.com>
2024-10-17 15:49:44 +0700

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as express from 'express';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -20,18 +19,19 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  const config = new DocumentBuilder()
-    .setTitle('Pricesenz Backend API Docs')
-    .setDescription('Pricesenz Backend API Docs and Structure')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-swagger', app, document);
-  const port = configService.get<number>('NEST_PORT');
-  await app.listen(port).then(() => {
-    console.log(`API server started on port http://localhost:${port}/api`);
-    console.log(`Swagger => http://localhost:${port}/api-swagger`);
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Pricesenz Backend API Docs')
+      .setDescription('Pricesenz Backend API Docs and Structure')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-swagger', app, document);
+  }
+
+  const port = process.env.PORT || configService.get<number>('NEST_PORT');
+  await app.listen(port);
 }
+
 bootstrap();
