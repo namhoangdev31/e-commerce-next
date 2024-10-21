0b1a9a75a208b4aeb3fca46d0dd7e0111c2a72cc
nammob <hoangnam@mobcec.com>
2024-10-21 17:00:34 +0700

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,20 @@
-import { GlobalModule } from './modules/global/global.module';
-import { HeaderModule } from './modules/header/header.module';
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { DatabaseModule } from './database/database.module';
-import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { StreamModule } from './modules/stream/stream.module';
-import { CallModule } from './modules/call/call.module';
-import { ViewsModule } from './modules/views/views.module';
-import { ClassModule } from './modules/class/class.module';
-import { UsersModule } from './modules/users/users.module';
-import { MessagesModule } from './modules/messages/messages.module';
-import { RolesModule } from './modules/roles/roles.module';
-import { RolesModule } from './modules/roles/roles.module';
+import { GlobalModule } from './modules/global/global.module'
+import { HeaderModule } from './modules/header/header.module'
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { DatabaseModule } from './database/database.module'
+import { SharedModule } from './shared/shared.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { StreamModule } from './modules/stream/stream.module'
+import { CallModule } from './modules/call/call.module'
+import { ViewsModule } from './modules/views/views.module'
+import { ClassModule } from './modules/class/class.module'
+import { UsersModule } from './modules/users/users.module'
+import { MessagesModule } from './modules/messages/messages.module'
+import { RolesModule } from './modules/roles/roles.module'
 
 @Module({
   imports: [
