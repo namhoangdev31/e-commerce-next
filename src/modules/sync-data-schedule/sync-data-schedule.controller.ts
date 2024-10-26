14fd6317f84b29a5249ac777dbd7cf64527b683d
namhndev <nguyenhoangnam31082000@gmail.com>
2024-10-26 21:33:36 +0700

    add swagger

--- src/modules/sync-data-schedule/sync-data-schedule.controller.ts
+++ src/modules/sync-data-schedule/sync-data-schedule.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
 import { SyncDataScheduleService } from './sync-data-schedule.service'
 import { ApiExcludeEndpoint } from '@nestjs/swagger'
+import { Public } from '../auth/decorators/public.decorator'
 
 @Controller('syncDataSchedule')
 export class SyncDataScheduleController {
@@ -8,6 +9,7 @@ export class SyncDataScheduleController {
 
   @Get()
   @ApiExcludeEndpoint()
+  @Public()
   syncData() {
     return this.syncDataScheduleService.syncDataURL()
   }
